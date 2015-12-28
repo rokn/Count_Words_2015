837950b146ba469236b3ef4d8f3defc3f70f1695
BassMuff <funkmetaljazzmaster@gmail.com>
2015-12-28 17:04:30 +0200

    Update src/word_counter.rb
    Now it can parse C/C++ strings properly

--- src/word_counter.rb
+++ src/word_counter.rb
@@ -6,6 +6,12 @@ class WordCounter
 	def self.parse_text text
 		text.gsub!(/[^a-zA-Z0-9]/,' ')
 	end
+
+	def self.get_strings_c text
+		strings = text.scan(/".*?"/)
+		text.gsub!(/".*?"/,' ')
+		strings
+	end
 	def self.parse_c text
 		#Still doesn't parse strings right
 		text.gsub!(/[-+=*\/;<>\(\){}&|,:\?\"\'\[\]\!]/,' ').gsub!(/\s[0-9][^\s]*/,'').gsub!(/\./,' ')
@@ -13,14 +19,20 @@ class WordCounter
 
 	public
 	def self.count_words text, ext
+		strings = Array.new
 		words = Hash.new
 		if ext == 'cpp' || ext == 'cc' || ext == 'c'
+			strings = get_strings_c text
 			parse_c text
 		else
 			parse_text text
 		end
 	  	text.downcase!
 		text.split(' ').each do |word|
+		  	words[word] = 0 if words[word]==nil
+		    words[word] += 1
+		end
+		strings.each do |word|
 		  	words[word] = 0 if words[word]==nil
 		    words[word] += 1
 		end
