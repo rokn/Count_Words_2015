23e013a4f2c15f9f81c2a98d60156c2977b73382
BassMuff <funkmetaljazzmaster@gmail.com>
2016-01-03 14:04:08 +0200

    Updated src/count_words.rb
    Add new argument - the language in which are the files
    Got tired writing good commit messages...

--- src/count_words.rb
+++ src/count_words.rb
@@ -5,32 +5,45 @@ def get_file_extension filename
 	split_fn.size == 1 ? '' : split_fn.last
 end
 
+def valid_language? str
+	str=='c' || str=='java' || str=='ruby'
+end
 
-if ARGV[0] == "--help" || ARGV.empty?
-	puts "Usage: ruby count_words.rb directory/filename output_file"
-	puts "First argument: the name of the input file or a directory"
-	puts "Second argument: the name of the output file"
-	puts "If only 1 argument is given, the program prints to the standart output"
+
+if ARGV[0] == "--help" || !valid_language?(ARGV[0]) || ARGV.size < 2
+	puts "Usage: ruby count_words.rb language directory/filename output_file"
+	puts "First argument: the language of the files to be parsed (eather c, java or ruby)"
+	puts "Second argument: the name of the input file or a directory"
+	puts "Third argument: the name of the output file"
+	puts "If the third argument is not given, the program prints to the standart output"
 else
-	output_filename = ARGV[1]
-	if File.file? ARGV[0]
+	target_language = ARGV[0].downcase
+
+	output_filename = ARGV[2]
+	if File.file? ARGV[1]
 		files = Array.new
-		files << ARGV[0]
+		files << ARGV[1]
 	else
-		files = Dir.glob(ARGV[0]+'**/*').select{ |e| File.file? e }
+		files = Dir.glob(ARGV[1]+'**/*').select{ |e| File.file? e }
 	end
 
 	counted_words = Hash.new
 
 	files.each do |filename|
 		puts "#{filename}" if output_filename != nil
-		extension = get_file_extension filename
+		ext = get_file_extension filename
 		
-		file = File.open(filename,"r")
-		text = String.new
-		file.each { |line| text << line }
-		
-		WordCounter.count_words text, extension, counted_words
+		type = String.new
+		type = 'c' if ext=='cpp' || ext=='cc' 
+		type = 'ruby' if ext=='rb'
+
+		if type == target_language
+			file = File.open(filename,"r")
+			text = String.new
+			file.each { |line| text << line }
+			
+			WordCounter.count_words text, type, counted_words
+		end
 	end
 
 	sorted_words = WordCounter.sort_words counted_words
