b8c1b888683d6c6f91793233d94b4405bbaa7468
vicky98 <vikiga98@gmail.com>
2016-01-10 18:59:52 +0200

    Update graphs.rb

--- svg_src/graphs.rb
+++ svg_src/graphs.rb
@@ -1,64 +1,80 @@
 require 'json'
 
 def make_square(x, y, w, h)
-	'<rect width="' + w.to_s + '" height="' + h.to_s + '" x = "' + x.to_s + '" y = "' + y.to_s+ '" style="fill:darkolivegreen;stroke-width:3;stroke:darkolivegreen"/>'
+    '<rect width="' + w.to_s + '" height="' + h.to_s + '" x = "' + x.to_s + '" y = "' + y.to_s+ '" style="fill:darkolivegreen;stroke-width:3;stroke:darkolivegreen"/>'
 end
 
 def largest_value(hash)
-	v = 0
+    v = 0
 
-	hash.each do |key, value| 
-		if !value.is_a? Numeric
-			value.each do |key1, value1|
-				if value1 > v
-					v = value1
-				end
-			end
-		end
-	end
+    hash.each do |key, value| 
+        if !value.is_a? Numeric
+            value.each do |key1, value1|
+                if value1 > v
+                    v = value1
+                end
+            end
+        end
+    end
 
-	return v
+    return v
 end
 
-def svg_gen(filename)
-	file = File.read(filename)
-	word_counts = Hash.new
-	word_counts = JSON.parse(file)
-	type = filename.split('.').first
+def words_c(hash)
+    w_ = 0
 
-	c = 1
-	width = 70
-	offset = 20
-	minWidth = 1.0
-	maxWidth = 300.0
-	maxCount = 0.0
-	         
-	min = minWidth.to_f
-	max = maxWidth.to_f
-	           
-	File.open('' + type.to_s + '.svg', "w") do |f|
-		f.write('<svg width = "10000" height = "500" xmlns="http://www.w3.org/2000/svg">')
+    hash.each do |key, value| 
+        if !value.is_a? Numeric
+            value.each do |key1, value1|
+                w_ = w_ + 1
+            end
+        end
+    end
 
-		maxCount = largest_value(word_counts)
-	        
-		word_counts.each do |word, count|
-			if count.is_a? Numeric
-				f.write('<text x="' + (c * (width + offset) + 15).to_s + '" y="' + (offset + maxWidth + 150).to_s + '">' + word + ': ' + count.to_s + '</text>')
-			else 
-				count.each do |word2, count2|
-					v = count2.to_f/maxCount.to_f
-					height = (max - min) * v
+    return w_
+end
 
-					f.write(make_square(c * (width + offset), offset + maxWidth - height, width, height))
-					f.write('<text x="' + (c * (width + offset) + 28).to_s + '" y="' + (offset + maxWidth - 10).to_s + '">' + count2.to_s + '</text>')
-					f.write('<text x="' + (c * (width + offset) + 35).to_s + '" y="' + (offset + maxWidth + 10).to_s + '" style="writing-mode: tb;">' + word2 + '</text>')
-					c = c + 1
-				end
-			end
-		end
+def svg_gen(filepath)
+    file = File.read(filepath)
+    word_counts = Hash.new
+    word_counts = JSON.parse(file)
+    filename = filepath.split('/').last
+    type = filename.split('.').first
 
-		f.write('</svg>')
-	end
+    c = 1
+    width = 70
+    offset = 20
+    minWidth = 1.0
+    maxWidth = 300.0
+    maxCount = 0.0
+    words2 = words_c(word_counts)
+
+    min = minWidth.to_f
+    max = maxWidth.to_f
+               
+    File.open('' + type.to_s + '.svg', "w") do |f|
+        f.write('<svg width = "' + (words2 * 100).to_s + '" height = "500" xmlns="http://www.w3.org/2000/svg">')
+
+        maxCount = largest_value(word_counts)
+            
+        word_counts.each do |word, count|
+            if count.is_a? Numeric
+                f.write('<text x="' + (c * (width + offset) + 15).to_s + '" y="' + (offset + maxWidth + 150).to_s + '">' + word + ': ' + count.to_s + '</text>')
+            else 
+                count.each do |word2, count2|
+                    v = count2.to_f/maxCount.to_f
+                    height = (max - min) * v
+
+                    f.write(make_square(c * (width + offset), offset + maxWidth - height, width, height))
+                    f.write('<text x="' + (c * (width + offset) + 28).to_s + '" y="' + (offset + maxWidth - 10).to_s + '">' + count2.to_s + '</text>')
+                    f.write('<text x="' + (c * (width + offset) + 35).to_s + '" y="' + (offset + maxWidth + 10).to_s + '" style="writing-mode: tb;">' + word2 + '</text>')
+                    c = c + 1
+                end
+            end
+        end
+
+        f.write('</svg>')
+    end
 end
 
 svg_gen(ARGV[0])
